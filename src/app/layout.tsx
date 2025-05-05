6bc6a4f3ea79fae57632b7ff5cc1551de2946538
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2025-05-06 03:43:46 +0600

    microsoft clirety

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -105,7 +105,7 @@ export default function RootLayout({
       c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
       t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
       y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "r7ozz862qv");
+    })(window, document, "clarity", "script", "rf06k8zlzo");
   `}
         </Script>
       </head>
